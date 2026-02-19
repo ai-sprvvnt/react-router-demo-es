a166ad5979d42e05a7dd0c8bd269f33729c702fe
Felipe-Garcia <ed.felipe.gn@gmail.com>
2026-02-19 09:55:45 -0600

    Configuración de router - reviews - aboutme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,22 @@
-import "./App.css";
-import Header from "./Header/Header";
+import { Routes, Route } from 'react-router-dom'; // Nueva importación
+import Dashboard from './Dashboard/Dashboard'; // Nueva importación
+import Reviews from './Reviews/Reviews'; // Nueva importación
+import AboutMe from './AboutMe/AboutMe'; // Nueva importación
+
+import './App.css';
+import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* Envuelve un componente Route dentro de un componente Routes  
+          y especifica la ruta y los atributos del elemento tal como se muestra. */}
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about-me" element={<AboutMe />} />
+      </Routes>
     </div>
   );
 }
