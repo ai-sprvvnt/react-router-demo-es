a43db180ee2f288b48bcf04147e38139f16a112c
Felipe-Garcia <ed.felipe.gn@gmail.com>
2026-02-20 13:00:12 -0600

    Rutas dinámicas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,53 @@
-//App.jsx
+import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+import Header from './Header/Header';
 import Dashboard from './Dashboard/Dashboard';
 import Reviews from './Reviews/Reviews';
-import Header from './Header/Header';
-import AboutUs from './AboutUs/AboutUs';
+import Review from './Review/Review';
 import AboutMe from './AboutMe/AboutMe';
-// Importa nuevos componentes
 import Contact from './AboutMe/Contact';
 import Hobbies from './AboutMe/Hobbies';
 import MyStory from './AboutMe/MyStory';
-
+import AboutUs from './AboutUs/AboutUs';
 import SiteHistory from './AboutUs/SiteHistory';
 import SiteMission from './AboutUs/SiteMission';
-
 import './App.css';
 
 function App() {
-  // Agrega las rutas hijo a la ruta /about-me.
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    // Obtener los datos de la reseña del servidor.
+    fetch('https://emoji-critic.es.tripleten-services.com/v1/reviews')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // Pasa el cuerpo de la respuesta analizada a la función setter.
+        setReviews(data);
+      })
+      .catch(console.error);
+    // Un array de dependencias vacío significa que el hook sólo se ejecuta cuando se inicia el componente.
+  }, []);
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/reviews" element={<Reviews reviews={reviews} />} />
+        <Route
+          path="/reviews/:reviewId"
+          element={<Review reviews={reviews} />}
+        />
         <Route path="/about-me" element={<AboutMe />}>
           <Route path="contact" element={<Contact />} />
           <Route path="hobbies" element={<Hobbies />} />
           <Route path="my-story" element={<MyStory />} />
         </Route>
-
         <Route path="/about-us" element={<AboutUs />}>
-          <Route path="site-history" element={<SiteHistory />} />
-          <Route path="site-mission" element={<SiteMission />} />
+          <Route path="mission" element={<SiteMission />} />
+          <Route path="history" element={<SiteHistory />} />
         </Route>
       </Routes>
     </div>
---
+++ src/components/Review/Review.jsx
@@ -0,0 +1,24 @@
+import { useParams } from 'react-router-dom';
+import './Review.css';
+
+function Review({ reviews }) {
+  const params = useParams();
+  let id = params.reviewId;
+  id = id - 1;
+
+  return (
+    <div className="review">
+      {reviews && (
+        <div className="review__item">
+          <h3>{reviews[id]?.title}</h3>
+          <p>{reviews[id]?.text}</p>
+          <p className="review__rating">
+            Calificación final:{reviews[id]?.rating}/5
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Review;
